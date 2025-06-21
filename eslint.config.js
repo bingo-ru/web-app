ce94b1ef506c87d33edf48a9eade7b4637c97737
tarkue <mrfrostlikchannel@gmail.com>
2025-06-21 13:01:42 +0500

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import solidjseslint from "eslint-plugin-solid";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  tseslint.configs.recommended,
+  solidjseslint.configs.typescript,
+]);
