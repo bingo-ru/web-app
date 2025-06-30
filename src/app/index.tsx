5b134549536cdf282705bf83720fc7360e98e56e
tarkue <mrfrostlikchannel@gmail.com>
2025-06-30 11:48:48 +0500

    add pre-commit hook

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import './index.css';
 import { render } from 'solid-js/web';
-import { Router, Route, RouteDefinition } from '@solidjs/router';
+import { Router } from '@solidjs/router';
 import { routerConfig } from './router';
 
 const root = document.getElementById('root');
